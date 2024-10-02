8819f0cfa38bd65c09062ad5b05a79cdcfeeddaa
Volgarenok <volgarenok@gmail.com>
2024-10-03 00:34:53 +0300

    Input flags

--- main.cpp
+++ main.cpp
@@ -3,6 +3,22 @@ int main()
 {
   int a = 0;
   std::cin >> a;
+  if (std::cin.good())
+  {
+    std::cerr << "Good!\n";
+  }
+  else if (std::cin.eof())
+  {
+    std::cerr << "Eof!\n";
+  }
+  else if (std::cin.bad())
+  {
+    std::cerr << "Bad!\n";
+  }
+  else if (std::cin.fail())
+  {
+    std::cerr << "Fail\n";
+  }
   std::cout << a;
   std::cout << "\n";
 }
